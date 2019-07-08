bd01f160ae5ad1a1f16815223a99dc73a5aa755e
Akihiro_Takano <superakip1995@gmail.com>
2019-07-09 00:14:07 +0900

    一時push

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registarations: 'users/registarations',
-    passwords:      'users/passwords',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    registarations:     'users/registarations',
+    passwords:          'users/passwords',
+    omniauth_callbacks: 'users/omniauth_callbacks' 
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
