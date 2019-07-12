8f56012fe74fd00a618892cd3199197799617285
uratomo <rop.ibuki@gmail.com>
2019-07-12 13:09:29 +0900

    コントローラ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,18 @@
 class ItemsController < ApplicationController
+  #トップページ　商品一覧
   def index
 
   end
 
+  #商品詳細ページ
   def show
   end
 
+  #商品購入確認ページ
+  def confirmation
+  end
+  
+  #商品出品ページ
   def new
     render layout: 'logo'
   end
@@ -13,13 +20,5 @@ class ItemsController < ApplicationController
   def create
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks' 
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items
+  
+  # itemsはusersにネストさせる
+  resources :items ,only: [:index,:show,:new,:create,]do
+    get "confirmation", on: :collection
+  end
   resources :users do
     resource :credit, only: [:show,:new,:create,:delete]#クレジットカードに必要なカラムによって、ルーティングが変わることがある。
   end
