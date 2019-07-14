a6913907b0e21839c52b66eb3cf75e6d042560a3
uratomo <rop.ibuki@gmail.com>
2019-07-14 17:02:43 +0900

    active_hash　モデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,28 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  has_many :image
+  belongs_to_active_hash :size
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_way
+  belongs_to_active_hash :shipping_payer
+  belongs_to_active_hash :shipping_address
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :condition
+
+  
+  validates :name,presence: true
+  validates :description,presence: true
+  validates :size_id,presence: true
+  validates :condition_id,presence: true
+  validates :shipping_payer_id,presence: true
+  validates :shipping_way_id,presence: true
+  validates :shipping_address_id,presence: true
+  validates :shipping_day_id,presence: true
+  validates :price,presence: true
+  # validates :category,presence: true
+  # validates :image,presence: true
+
+
+
 end
---
+++ app/models/shipping_way.rb
@@ -0,0 +1,13 @@
+class ShippingWay < ActiveHash::Base
+  self.data = [
+{id:"1",way:"未定"},
+{id:"2",way:"らくらくメルカリ便"},
+{id:"3",way:"ゆうメール"},
+{id:"4",way:"レターパック"},
+{id:"5",way:"普通郵便(定形、定形外)"},
+{id:"6",way:"クロネコヤマト"},
+{id:"7",way:"ゆうパック"},
+{id:"8",way:"クリックポスト"},
+{id:"9",way:"ゆうパケット"},
+  ]
+end
