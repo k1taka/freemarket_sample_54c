06ee33de41db89198242e6da3227707801318647
uratomo <rop.ibuki@gmail.com>
2019-07-14 17:03:32 +0900

    出品ルーティング作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item_new,only: [:new,:create]
+
   #トップページ　商品一覧
   def index
 
@@ -14,22 +16,41 @@ class ItemsController < ApplicationController
   
   #商品出品ページ
   def new
+    @item = Item.new
+    @sizes = Size.all
+    @conditions = Condition.all
+    @shipping_payers = ShippingPayer.all
+    @shipping_way = ShippingWay.all
+    @shipping_address = ShippingAddress.all
+    @shipping_day = ShippingDay.all
     render layout: 'logo'
   end
   
   def create
-    redirect_to :index
+    @item =Item.new(item_params)
+  if @item.save
+    redirect_to root_path, notice: 'グループを作成しました'
+  else
+    render :new
+  end
   end
 
 
   private
 
   def item_params
-    params.require(:items).permit(:content, :image).merge(user_id: current_user.id) #permit部分は編集すること
+    params.require(:item).permit(:name, :description, :size_id,:brand,:condition_id,:shipping_payer_id,:shipping_way_id,:shipping_address_id,:shipping_day_id,:price)
+    #.merge(user_id: current_user.id)
+    #:category  :image seller_id
   end
 
-  def set_item
-    @item=Item.find(params[:item])  #リファクタリング
+  def set_item_new #createにも必要　validateでエラーが出る
+    @sizes = Size.all
+    @conditions = Condition.all
+    @shipping_payers = ShippingPayer.all
+    @shipping_way = ShippingWay.all
+    @shipping_address = ShippingAddress.all
+    @shipping_day = ShippingDay.all
   end
 
 end
