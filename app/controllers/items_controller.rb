4d7d1f7379375cc6e593bc6226b2f2384c05eb47
49497974m <49497974tkhsm>
2019-07-16 20:15:59 +0900

    APIキーと商品価格記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,9 +16,9 @@ class ItemsController < ApplicationController
 
   # クレジットカード決済のカード情報記入ページ
   def pay
-    Payjp.api_key = PAYJP_SECRET_KEY
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: @item.price, # 決済する値段
+      amount: 2000, # 決済する値段
       card: params['payjp-token'],
       currency: 'jpy'
     )
