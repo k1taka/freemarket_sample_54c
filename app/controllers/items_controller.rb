f0a0c8ec2d4795817afd64c5ef38732d06e00f07
uratomo <rop.ibuki@gmail.com>
2019-07-16 12:11:33 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   
   def create
     @item =Item.new(item_params)
-    binding.pry
   if @item.save
     redirect_to root_path, notice: 'グループを作成しました'
   else
