557116177657f97def63ee335f71218f227084e3
k1taka <sofmori@gmail.com>
2019-07-18 12:48:43 +0900

    ハッシュ形式に修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
     # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
     # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    aws_secret_access_key: Rails.application.credentials.aws[secret_access_key],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
     region: 'ap-northeast-1'
   }
 
