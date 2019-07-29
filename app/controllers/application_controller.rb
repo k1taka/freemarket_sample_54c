1cb18316b145004f1b51cde7f10bb679a0e489bd
uratomo <rop.ibuki@gmail.com>
2019-07-29 17:25:51 +0900

    authenticate_user? 実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
 protect_from_forgery with: :exception #セキュリティ
-# before_action :authenticate_user!  サインイン完成させないとログインできないはず。
+before_action :authenticate_user! , except: [:index,:show]
 before_action :basic_auth , if: :production?
 before_action :configure_permitted_parameters, if: :devise_controller?
 before_action :category_field
